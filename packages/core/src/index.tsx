c7f73fee1cebba33b6ee4e0a85ba0eb90a3531df
suanmei <mr_suanmei@163.com>
2020-04-23 14:18:16 +0800

    feat: 本地开发持续构建

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react';
 
 const Core = (): JSX.Element => {
-  return <div>2</div>;
+  return (
+    <div>
+      2
+      <span role="img" aria-label="ok">
+        👌
+      </span>
+      2
+    </div>
+  );
 };
 
 export default Core;
