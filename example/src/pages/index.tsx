935e47029de47b6999eff03e8a9ee235efe2f64b
smallstonesk <575913857@qq.com>
2020-09-07 09:46:52 +0800

    chore: remove useless dependencies

--- example/src/pages/index.tsx
+++ example/src/pages/index.tsx
@@ -1,20 +1,15 @@
 import * as React from 'react';
 import IMove from '@imove/core';
-import styled from '@emotion/styled';
 
 const onSave = (data: { nodes: any; edges: any }): void => {
   console.log(data);
 };
 
-const Wrapper = styled.div`
-  height: 100vh;
-`;
-
 function Arrange(): JSX.Element {
   return (
-    <Wrapper>
+    <div style={{height: '100vh'}}>
       <IMove onSave={onSave}/>
-    </Wrapper>
+    </div>
   );
 }
 
