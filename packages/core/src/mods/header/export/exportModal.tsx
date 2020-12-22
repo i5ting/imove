5823fbafb18e3261ff98bebf8bdb2a2b7bb5cfdc
smallstonesk <575913857@qq.com>
2020-12-22 19:32:24 +0800

    feat: add compiling code online

--- packages/core/src/mods/header/export/exportModal.tsx
+++ packages/core/src/mods/header/export/exportModal.tsx
@@ -3,8 +3,10 @@ import React from 'react';
 import 'antd/es/modal/style';
 import styles from './index.module.less';
 
+import JSZip from 'jszip';
 import { Modal } from 'antd';
 import { Graph } from '@antv/x6';
+import compileCode from '@imove/compile-code';
 import { base64Url2Blob, downloadFile } from '../../../utils';
 
 interface IExportModalProps {
@@ -21,6 +23,15 @@ const ExportModal: React.FC<IExportModalProps> = (props) => {
     const blob = new Blob([dsl], { type: 'text/plain' });
     downloadFile('imove.dsl.json', blob);
   };
+  const onExportCode = () => {
+    const zip = new JSZip();
+    const dsl = flowChart.toJSON();
+    const output = compileCode(dsl);
+    Helper.recursiveZip(zip, output);
+    zip.generateAsync({ type: 'blob' }).then((blob) => {
+      downloadFile('logic.zip', blob);
+    });
+  };
   const onExportFlowChart = () => {
     flowChart.toJPEG((dataUri: string) => {
       const blob = base64Url2Blob(dataUri);
@@ -30,7 +41,7 @@ const ExportModal: React.FC<IExportModalProps> = (props) => {
 
   return (
     <Modal
-      className={styles.editModal}
+      width={700}
       visible={visible}
       footer={null}
       title={'导出'}
@@ -39,16 +50,37 @@ const ExportModal: React.FC<IExportModalProps> = (props) => {
     >
       <div className={styles.modalContent}>
         <div className={styles.downloadWrap} onClick={onExportDSL}>
-          <img src="//gw.alicdn.com/tfs/TB1FaTkuqNj0u4jSZFyXXXgMVXa-128-128.png" />
+          <img className={styles.picPreview} src="//img.alicdn.com/imgextra/i2/O1CN01lvanDL1YKp54hGgMS_!!6000000003041-2-tps-247-247.png" />
           <span>DSL</span>
         </div>
+        <div className={styles.downloadWrap} onClick={onExportCode}>
+          <img className={styles.picPreview} src="//img.alicdn.com/imgextra/i3/O1CN01V3wg6S27JuqePTXZv_!!6000000007777-2-tps-247-247.png" />
+          <span>代码</span>
+        </div>
         <div className={styles.downloadWrap} onClick={onExportFlowChart}>
-          <img src="//gw.alicdn.com/tfs/TB1WZeT4Hr1gK0jSZFDXXb9yVXa-128-128.png" />
+          <img className={styles.picPreview} src="//img.alicdn.com/imgextra/i1/O1CN01sVItyC1exeLraUhf6_!!6000000003938-2-tps-247-247.png" />
           <span>流程图</span>
         </div>
       </div>
     </Modal>
   );
 };
+
+const Helper = {
+  recursiveZip(root: JSZip, json: any) {
+    if (typeof json !== 'object' || json === null) {
+      return;
+    }
+    for (const key in json) {
+      const val = json[key];
+      if (typeof val === 'string') {
+        root.file(key, val);
+      } else {
+        const dir = root.folder(key) as JSZip;
+        Helper.recursiveZip(dir, val);
+      }
+    }
+  }
+}
 
 export default ExportModal;
