4a1614b4ef8b0372af4ca43c58beeb706b2cb241
yangpei <2517625275@qq.com>
2021-02-04 11:43:40 +0800

    feat: add article links in help document

--- packages/core/src/mods/header/guide/guideModal.tsx
+++ packages/core/src/mods/header/guide/guideModal.tsx
@@ -310,31 +310,47 @@ const Document: React.FC = () => {
           target="blank"
           href="https://github.com/imgcook/imove/blob/master/README.md"
         >
-          github README
+          github README.md
         </a>
       </div>
       <div>
         <a
           target="blank"
-          href="https://mp.weixin.qq.com/s/cfGdGp1VAVn_bUqXUspvJg"
+          href="https://www.yuque.com/imove/blog/bee1ki"
         >
-          登上 github 趋势榜，iMove 原理技术大揭秘!
+          2021年前端趋势预测
         </a>
       </div>
       <div>
         <a
           target="blank"
-          href="https://www.infoq.cn/article/hMVR8kJvRhXxod8ydiBt"
+          href="https://www.yuque.com/imove/blog/tcdrps"
         >
-          imove基于 x6 + form-render 背后的思考
+          F2C能否让前端像运营配置一样开发？
         </a>
       </div>
       <div>
         <a
           target="blank"
-          href="https://mp.weixin.qq.com/s/tqZaZTTrjZlo9T7f9sLNIA"
+          href="https://www.yuque.com/imove/blog/qxq2mw"
         >
-          "所见即所得!" iMove 在线执行代码探索
+          登上 Github 趋势榜，iMove 原理技术大揭秘!
+        </a>
+      </div>
+      <div>
+        <a
+          target="blank"
+          href="https://www.yuque.com/imove/blog/ettxi8"
+        >
+          iMove 基于 X6 + form-render 背后的思考
+        </a>
+      </div>
+      <div>
+        <a
+          target="blank"
+          href="https://www.yuque.com/imove/blog/vgp6bb"
+        >
+          所见即所得! iMove 在线执行代码探索
         </a>
       </div>
     </div>
