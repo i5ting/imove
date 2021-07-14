0d60cd5259a3a4811a58611109d1b78f66dde4cd
Hans Chan <hanchai.chc@alibaba-inc.com>
2021-07-14 15:43:40 +0800

    feat(cli): add cli-editor to open local editor

---
+++ packages/cli/src/cmd/editor/build.js
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const { lessLoader } = require('esbuild-plugin-less');
+
+require('esbuild')
+  .build({
+    bundle: true,
+    entryPoints: [path.join(__dirname, 'template/app.jsx')],
+    outfile: path.join(__dirname, 'template/app.bundle.js'),
+    plugins: [
+      {
+        name: 'resolve-antd-dist-less',
+        setup: (build) => {
+          build.onResolve(
+            { filter: /antd\/dist\/antd\.less$/, namespace: 'file' },
+            () => {
+              return {
+                path: '',
+                watchFiles: undefined,
+              };
+            },
+          );
+        },
+      },
+      lessLoader({
+        javascriptEnabled: true,
+      }),
+    ],
+  })
+  .catch(() => process.exit(1));
